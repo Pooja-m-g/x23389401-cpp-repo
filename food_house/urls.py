b0ec1275ff10c5568f2ee34b06be044861751715
Pooja-m-g <x23389401@student.ncirl.ie>
2026-07-30 16:14:00 +0530

    Update urls.py

--- food_house/urls.py
+++ food_house/urls.py
@@ -15,10 +15,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view,name=''),
     path('', views.home_view, name='home'),
-    path('afterlogin', views.afterlogin_view,name='afterlogin'),
-    # path('logout', LogoutView.as_view(template_name='ecom/logout.html'),name='logout'),
-    # path('logout/', views.logout_view, name='logout'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+
     #path('aboutus', views.aboutus_view),
     #path('contactus', views.contactus_view,name='contactus'),
     # Use custom logout view
